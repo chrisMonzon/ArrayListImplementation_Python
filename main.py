fed9a93ae18ee55eff75ee416a4574603a654eb5
Monzon <Monzon@JMs-MacBook-Air.local>
2019-02-02 09:59:01 -0800

    removing the old files

--- main.py
+++
@@ -1,9 +0,0 @@
-from person import Person
-from employee import Employee
-
-x = Person("Marge", "Simpson")
-y = Employee("Homer", "Simpson", "1007")
-
-print(x.Name())
-print(y.GetEmployee())
-print(y.Name())
